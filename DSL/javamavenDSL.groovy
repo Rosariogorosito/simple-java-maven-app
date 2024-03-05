ca9371f53223d712b91928a14f7fc1c8b5c4d985
Rosariogorosito <101011246+Rosariogorosito@users.noreply.github.com>
2024-03-04 22:38:37 -0300

    Update javamavenDSL.groovy

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/Rosariogorosito/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('Rosariogorosito')
+            node / gitConfigEmail('aldanagorosito802@gmail.com')
         }
     }
     steps {
